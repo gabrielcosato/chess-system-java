b20fe6db472dbdf7885cf47fd0bd716188103f8e
Gabriel Cosato <gabrielcosato@gmail.com>
2024-05-25 04:58:18 -0300

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -2,7 +2,11 @@ package application;
 
 import chess.ChessMatch;
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -49,5 +53,19 @@ public class UI {
             }
         }
         System.out.print(" ");
+    }
+
+    public static ChessPosition readChessPosition(Scanner sc){
+        try {
+
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+
+            return new ChessPosition(column, row);
+        }
+        catch (RuntimeException e){
+            throw new InputMismatchException("Error reading ChessPosition, Valid values are from a1 to h8.");
+        }
     }
 }
